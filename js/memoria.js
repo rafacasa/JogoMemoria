da71e2a2786214c89b9bc4837358fe4a61f4527d
Rafael <rafaelcasa.2007@yahoo.com.br>
2016-05-13 11:30:06 -0300

    Separar funcao _GET para outro arquivo

--- js/memoria.js
+++ js/memoria.js
@@ -1,19 +1,3 @@
-//https://xanadu1010.wordpress.com/2013/03/09/usando-javascript-para-pegar-valores-enviados-a-pagina-html-via-get/
-		function _GET(name)
-		{
-		  var url   = window.location.search.replace("?", "");
-		  var itens = url.split("&");
-
-		  for(n in itens)
-		  {
-		    if( itens[n].match(name) )
-		    {
-		      return decodeURIComponent(itens[n].replace(name+"=", ""));
-		    }
-		  }
-		  return null;
-		}
-
 		//http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 		function shuffle(array) {
 			var currentIndex = array.length, temporaryValue, randomIndex;
