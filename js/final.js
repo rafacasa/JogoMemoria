ce97d2ca4b5efce26df6abf8937514b459dddcd3
Rafael Luiz Casa <rafaelcasa.2007@yahoo.com.br>
2016-06-05 11:20:15 -0300

    corrigir erro final.js

--- js/final.js
+++ js/final.js
@@ -1,3 +1,17 @@
+//https://xanadu1010.wordpress.com/2013/03/09/usando-javascript-para-pegar-valores-enviados-a-pagina-html-via-get/
+function _GET(name)
+{
+	var url   = window.location.search.replace("?", "");
+	var itens = url.split("&");
+
+	for(var n in itens) {
+		if( itens[n].match(name) ) {
+			return decodeURIComponent(itens[n].replace(name+"=", ""));
+		}
+	}
+	return null;
+}
+
 function ganhador() {
 	var nomeGanhador = _GET("ganhador");
 	if(nomeGanhador !== "empate") {
